31b16b05de06db356c90db40a3de64fea13fe698
Rohit George <rohitgeorge2000@gmail.com>
2021-01-10 13:16:44 +0530

    port2

--- project/src/App.js
+++ project/src/App.js
@@ -1,18 +1,11 @@
-import logo from "./images/workspaces-24px (1).svg";
 import "./App.css";
 import Exhead from "./Exhead.js";
 import React from "react";
-import Exfoot from "./Exfoot.js";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Exhead></Exhead>
-      <header class="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p class="para">DON'T JUST SURVIVE, THRIVE!!</p>
-      </header>
-      <Exfoot></Exfoot>
     </div>
   );
 }
--- project/src/Exhead.js
+++ project/src/Exhead.js
@@ -1,15 +1,63 @@
 import React from "react";
 import "./App.css";
 import logo from "./images/rgt.png";
+import logo1 from "./images/IMG_8094.JPG";
 
 function Exhead() {
   return (
     <div class="head">
-      <img src={logo} className="image" alt="logo" />
-      <div class="container">
+      <div class="hom" id="HOME">
+        <div class="header">
+          <img src={logo} className="image" alt="logo" />
+          <div class="container">
+            <div class="navbar">
+              <a href="#HOME">Home </a>
+              <a href="#ABOUTME">About Me</a>
+              <a href="#CAREER">Career</a>
+              <a href="#CONTACTME">Contact Me</a>
+            </div>
+          </div>
+          <div class="body">
+            <p class="pbody">Hi, All!!</p>
+            <p class="pbody">Welcome</p>
+          </div>
+        </div>
+      </div>
+      <div class="about" id="ABOUTME">
         <div class="navbar">
-          <a href="#HOME">HOME </a>
-          <a href="#ABOUT">ABOUT </a>
+          <a href="#HOME">Home </a>
+          <a href="#ABOUTME">About Me</a>
+          <a href="#CAREER">Career</a>
+          <a href="#CONTACTME">Contact Me</a>
+        </div>
+        <div class="am">
+          <img src={logo1} class="myimg" alt="me" />
+          <div class="bar2">
+            <h1>About Me</h1>
+            <p class="bio">
+              Hi All. I'm Rohit George, a sophomore at Federal Institute of
+              Science and Technology, undergoing the course B.Tech Computer
+              Science and Engineering.
+            </p>
+            <p class="bio">
+              An upcoming CS Engineering Student aspiring to be an experienced
+              Full Stack Developer in the coming years.
+            </p>
+            <p class="bio">
+              Had an interest in the field of Game Develpoment. Started learning
+              and working on it and at the end failed.
+            </p>
+            <p class="bio">
+              Thought of moving onto Web Development using Reactjs under the
+              guidance of some amazing seniors, and now, trying to learn more in
+              this field.
+            </p>
+            <p class="bio">
+              Also working on App Development using React-Native, being mentored
+              by some experienced seniors.
+            </p>
+            <p class="bio">Member of IEEE FISAT SB since 2019.</p>
+          </div>
         </div>
       </div>
     </div>
